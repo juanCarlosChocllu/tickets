336d6380558f411c756d0118ed9784f0f292aba4
Juan carlos chocllu <j.chocllu@opti-system.com>
2024-07-26 16:48:49 -0400

    hola

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({})
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  const config = new DocumentBuilder()
+    .setTitle('Tickets ')
+    .setDescription('sistema de tickets')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(3000);
+
+
 }
+
+
 bootstrap();
--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -10,6 +10,6 @@ export class CreateUsuarioDto {
     usuario:string
     @IsString()
     contrasena:string
-
-
+    
+    
 }
